a860b5bada6c244a64d698947eb0f66ece841374
dgenovese1 <dangenovese123@gmail.com>
2022-06-12 09:57:46 -0400

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,14 @@ import React from "react";
 const Form = () => {
     return (
         <div className="form">
-            <form action="https://public.herotofu.com/v1/45074a50-e9b9-11ec-94cd-436cd72d679b" method="post">
+            <form action="https://submit-form.com/FVJpxM2a">
                     <label for="name">Your Name</label>
                     <input name="Name" id="name" type="text" required />
                     <label for="email">Your Email</label>
                     <input name="Email" id="email" type="email" required />
                     <label>Message</label>
-                    <textarea rows="6" placeholder="Enter your message here"></textarea>
-                    <input className="btn" type="submit" value="Send" />
+                    <textarea name="message" rows="6" placeholder="Enter your message here"></textarea>
+                    <input className="btn" type="submit" value="Send" required/>
             </form>
         </div>
     );
